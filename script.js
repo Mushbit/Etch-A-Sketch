088715a28f87a407a5782dc0cbdff22c5642598e
Max <mushbit@protonmail.com>
2022-08-16 10:36:47 +0200

    The grid is working. promt for gridsize. random bg-color on hover. Working on fade out.

--- script.js
+++ script.js
@@ -1,13 +1,56 @@
-let main = document.querySelector('#main');
+let container = document.querySelector('.container');
+let button = document.querySelector('.button');
+button.addEventListener('click', askGridSize);
+
+function askGridSize() {
+
+    if (document.querySelectorAll('.row')) {
+
+        let rows = document.querySelectorAll('.row');
+    
+        rows.forEach( row => container.removeChild(row) );
+    }
+    createGrid(prompt('How many tiles squared would you like your grid to be?', ''));
+};
 
 
+function createGrid(number) {
 
+    if (number > 100) {
+        
+        number = prompt('We can only handle 100x100 tiles max', '');
 
+    } else {
 
+        dimensionTile = 1000 / number;
 
-for (let i = 0; i < 256; i++) {
-    let tile = document.createElement('div');
-    main.appendChild(tile);
-    tile.setAttribute('class', 'tile')
+        for (let i = 0; i < number; i++) {
+            let row = document.createElement('div');
+            container.appendChild(row);
+            row.setAttribute('class', 'row');
+
+            for (let i = 0; i < number; i++) {
+                let tile = document.createElement('div');
+                row.appendChild(tile);
+                tile.classList.add('tile', 'opacity');
+                tile.addEventListener('mouseover', setInterval(addRGB), 50)
+                //tile.addEventListener('mouseout', removeRGB)
+            }
+        let tiles = document.querySelectorAll('.tile');
+        tiles.forEach(tile => tile.setAttribute)
+        }
+    }
 }
 
+function removeRGB(event) {
+    event.target.classList.toggle('opacity');
+}
+
+function addRGB(event) {
+    const rgb = [];
+    for (let i = 0; i < 3; i++) {
+        rgb[i] = Math.floor(Math.random() * 255) +1;
+    }
+   event.target.style.backgroundColor = 'rgb(' + rgb.join(', ') + ')';
+   event.target.classList.toggle('opacity');
+}
